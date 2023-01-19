25466e7074aac9d9b8f646405d4177a0f87f420a
yuyizhong <zoe_yyz@hotmail.com>
2023-01-19 11:42:30 +0000

    Get last 5 items in the column

--- run.py
+++ run.py
@@ -104,6 +104,24 @@ def update_worksheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    print(columns)
+    
+
+    column = sales.col_values(3)
+    print(column)
+
 
 def main():
     """
@@ -117,4 +135,4 @@ def main():
     
 print("welcome to Love Sandwiches Data Automation")
 
-main()
+get_last_5_entries_sales()
