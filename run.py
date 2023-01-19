a5072cbd08873bf5e82c5b6273f67503cd56c117
yuyizhong <zoe_yyz@hotmail.com>
2023-01-19 00:23:10 +0000

    Add caculation for surplus

--- run.py
+++ run.py
@@ -70,12 +70,20 @@ def update_sales_worksheet(data):
 
 def caculate_surplus_data(sales_row):
     """
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
     print("Caculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
-
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    return surplus_data
 
 def main():
     """
@@ -84,7 +92,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet (sales_data)
-    caculate_surplus_data(sales_data)
+    new_surplus_data = caculate_surplus_data(sales_data)
+    print(new_surplus_data)
 print("welcome to Love Sandwiches Data Automation")
 
 main()
